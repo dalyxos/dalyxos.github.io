a3df89a5e32fc8ad4922a1bfc247385ee4503e34
Mohamed-Ali Daghfous <med.ali.daghfous@codencia.com>
2021-04-25 01:44:19 +0100

    Updates

---
+++ precache-manifest.7b340a7eccf57580373333bc4976755f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1e0de6f17f410582543400fb0218b5d",
+    "url": "/dalyxos.github.io/index.html"
+  },
+  {
+    "revision": "4c81c40bf8cb8eeeeb6b",
+    "url": "/dalyxos.github.io/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "8934441f503c467ae413",
+    "url": "/dalyxos.github.io/static/css/main.40f0e6cc.chunk.css"
+  },
+  {
+    "revision": "4c81c40bf8cb8eeeeb6b",
+    "url": "/dalyxos.github.io/static/js/2.b05d3167.chunk.js"
+  },
+  {
+    "revision": "bc645b8808bddaa3c0c8d72dff721b3b",
+    "url": "/dalyxos.github.io/static/js/2.b05d3167.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8934441f503c467ae413",
+    "url": "/dalyxos.github.io/static/js/main.91cfecb3.chunk.js"
+  },
+  {
+    "revision": "09e7c868213613403f5d",
+    "url": "/dalyxos.github.io/static/js/runtime-main.6cfa6629.js"
+  },
+  {
+    "revision": "9d39f883762c321cff929915bb06a051",
+    "url": "/dalyxos.github.io/static/media/dalyxos.9d39f883.png"
+  },
+  {
+    "revision": "c71b23920826dbc8cdd792370a7efb68",
+    "url": "/dalyxos.github.io/static/media/dx_logo.c71b2392.png"
+  }
+]);
